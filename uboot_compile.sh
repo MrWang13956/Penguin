c31f87d70e23590e26c0b63971add66b64df27b3
Weiye Wang <wwytxjy@163.com>
2022-05-15 16:03:46 +0800

    Add new dts and deconfig for WYL1D
    Add suniv-f1c100s-wyl1d.dts wyl1d_uboot_defconfig
    and uboot_compile.sh.
    
    Signed-off-by: Weiye Wang <wwytxjy@163.com>

---
+++ uboot_compile.sh
@@ -0,0 +1,23 @@
+#/bin/bash
+
+MAKE_CONFIG="make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- wyl1d_uboot_defconfig -j16"
+MAKE_UBOOT="make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- -j16"
+CLEAN_UBOOT="make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- distclean -j16"
+
+if [ $1 = "-h" ]
+then
+	echo "-m make uboot"
+	echo "-c clean uboot"
+
+elif [ $1 = "-c" ]
+then
+	$CLEAN_UBOOT
+
+elif [ $1 = "-m" ]
+then
+	$MAKE_CONFIG && $MAKE_UBOOT
+
+else
+	echo "Please enter -h for help!"
+
+fi
