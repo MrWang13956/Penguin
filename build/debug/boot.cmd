8093e61d09e67b81610705cb2d006945baa318f0
Weiye Wang <wwytxjy@163.com>
2022-05-20 23:13:35 +0800

    Make uart1 the kernel default serial port
    Modify console to tty1 in boot.cmd,
    make uart1 the kernel default serial port.
    
    Signed-off-by: Weiye Wang <wwytxjy@163.com>

--- build/debug/boot.cmd
+++ build/debug/boot.cmd
@@ -1,2 +1,2 @@
-setenv bootargs console=tty0 console=ttyS0,115200 panic=5 rootwait root=/dev/mmcblk0p2 rw 
+setenv bootargs console=tty1 console=ttyS1,115200 panic=5 rootwait root=/dev/mmcblk0p2 rw
 bootz 0x80008000 - 0x80C00000
--- build/gen_scr.sh
+++ build/gen_scr.sh
@@ -7,7 +7,7 @@ cd $SCRIPT_DIR
 if [ "$_BOOT_DEV" = tf ]; then  #tf
 (
 cat << EOF
-setenv bootargs console=tty0 console=ttyS0,115200 panic=5 rootwait root=/dev/mmcblk0p2 rw 
+setenv bootargs console=tty1 console=ttyS1,115200 panic=5 rootwait root=/dev/mmcblk0p2 rw
 load mmc 0:1 0x80C00000 $_DTB_NAME
 load mmc 0:1 0x80008000 zImage
 bootz 0x80008000 - 0x80C00000
