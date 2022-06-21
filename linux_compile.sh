bf6d2796329df42c9dbef25e7d8ef4df5c285d78
Weiye Wang <wwytxjy@163.com>
2022-06-21 22:44:38 +0800

    Add make modules in linux_compile.sh
    Signed-off-by: Weiye Wang <wwytxjy@163.com>

--- linux_compile.sh
+++ linux_compile.sh
@@ -3,6 +3,7 @@
 MAKE_CONFIG="make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- penguin_defconfig -j16"
 MAKE_LINUX="make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- -j16"
 MAKE_DTBS="make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- dtbs -j16"
+MAKE_MODULES="make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- INSTALL_MOD_PATH=../setup/build/output/ modules_install"
 CLEAN_LINUX="make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- distclean -j16"
 
 if [ $1 = "-h" ]
@@ -18,7 +19,7 @@ elif [ $1 = "-m" ]
 then
 	if [ $2 = "linux" ]
 	then
-		$MAKE_CONFIG && $MAKE_LINUX
+		$MAKE_CONFIG && $MAKE_LINUX && $MAKE_MODULES
 	elif [ $2 = "dtbs" ]
 	then
 		$MAKE_CONFIG && $MAKE_DTBS
